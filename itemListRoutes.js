3fd63989f5db7a92a687421e601d26d955783790
Michael Aylmer <michaelaylmer@Michaels-MacBook-Air.local>
2023-08-31 13:52:26 -0400

    wrote logic for PATCH, DELETE request, and for supertest file.

--- itemListRoutes.js
+++ itemListRoutes.js
@@ -11,11 +11,11 @@ router.get('/', function (req, res) {
 
 // POST route for /items.
 router.post('/', function (req, res) {
-    const newItem = [
-        { "name": "popsicle", "price": 1.45 },
-        { "name": "cheetos", "price": 3.40 }]
-    items.push(newItem)
-    res.status(201).json({ item: newItem })
+    const newItem1 = { "name": "popsicle", "price": 1.45 };
+    const newItem2 = { "name": "cheetos", "price": 3.40 };
+    items.push(newItem1);
+    items.push(newItem2);
+    res.status(201).json({ item: newItem1, newItem2 })
 })
 
 // GET route for item by specific name.
@@ -29,7 +29,7 @@ router.get('/:name', function (req, res) {
 
 // PATCH route for items.
 router.patch('/:name', function (req, res) {
-    const findItem = items.find(item => item.name = req.params.name)
+    const findItem = items.find(item => item.name === req.params.name);
     if (findItem === undefined) {
         throw new ExpressError("Item not found", 404)
     }
@@ -37,6 +37,15 @@ router.patch('/:name', function (req, res) {
     res.json({ item: findItem })
 })
 
+// DELETE route
+router.delete('/:name', function (req, res) {
+    const findItemIndex = items.findIndex(item => item.name === req.params.name);
+    if (findItemIndex === -1) {
+        throw new ExpressError("Item not found", 404);
+    }
+    items.splice(findItemIndex, 1);
+    res.json({ msg: "Deleted" });
+});
 
 // export router
 module.exports = router;
