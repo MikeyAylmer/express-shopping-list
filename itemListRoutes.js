e92ad85465fe8590fb04b95fe7f10b2cd512a1ae
Michael Aylmer <michaelaylmer@Michaels-MacBook-Air.local>
2023-08-31 13:30:28 -0400

    define routes for finding item by name & post & get route.

--- itemListRoutes.js
+++ itemListRoutes.js
@@ -1,19 +1,30 @@
 const express = require('express');
+const ExpressError = require('./expressError');
 const router = new express.Router();
 const items = require('./fakeDb');
 
-// define post routes
 
+// GET route for /items.
 router.get('/', function (req, res) {
     res.json({ items })
 })
 
+// POST route for /items.
 router.post('/', function (req, res) {
     const newItem = [
         { "name": "popsicle", "price": 1.45 },
         { "name": "cheetos", "price": 3.40 }]
     items.push(newItem)
     res.status(201).json({ item: newItem })
+})
+
+// GET route for item by specific name.
+router.get('/:name', function (req, res) {
+    const findItem = items.find(item => item.name == req.params.name)
+    if (findItem == undefined) {
+        throw new ExpressError("Item Not Found", 404)
+    }
+    res.json({ item: findItem })
 })
 
 module.exports = router;
