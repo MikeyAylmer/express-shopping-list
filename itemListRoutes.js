05e243373c8a7bef866fb1092999b507fa8e4395
Michael Aylmer <michaelaylmer@Michaels-MacBook-Air.local>
2023-08-31 13:15:21 -0400

    define routes for get request items. also imported morgan and nodemon.

--- itemListRoutes.js
+++ itemListRoutes.js
@@ -1,2 +1,19 @@
 const express = require('express');
 const router = new express.Router();
+const items = require('./fakeDb');
+
+// define post routes
+
+router.get('/', function (req, res) {
+    res.json({ items })
+})
+
+router.post('/', function (req, res) {
+    const newItem = [
+        { "name": "popsicle", "price": 1.45 },
+        { "name": "cheetos", "price": 3.40 }]
+    items.push(newItem)
+    res.status(201).json({ item: newItem })
+})
+
+module.exports = router;
